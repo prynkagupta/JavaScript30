12b9abc4912d7ddb83f3594f698ed0d2c471fed7
prynkagupta <priyanka2002.pkg@gmail.com>
2026-01-08 23:46:15 +0530

    Q09/30

---
+++ questions/ReturnLengthofArgumentsPassed.js
@@ -0,0 +1,7 @@
+//Q09/30 - 2703. Return Length of Arguments Passed
+
+//Write a function argumentsLength that returns the count of arguments passed to it.
+var argumentsLength = function(...args) {
+    return args.length
+    
+};
