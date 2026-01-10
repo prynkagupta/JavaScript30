6006434b69f92eda0b55a2388bef71c6bf08a607
prynkagupta <priyanka2002.pkg@gmail.com>
2026-01-10 23:22:03 +0530

    Q11/30

---
+++ questions/memoize.js
@@ -0,0 +1,28 @@
+/*
+Q.11/30 2623 Memoize
+Given a function fn, return a memoized version of that function.
+
+A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+You can assume there are 3 possible input functions: sum, fib, and factorial.
+
+sum accepts two integers a and b and returns a + b. Assume that if a value has already been cached for the arguments (b, a) where a != b, it cannot be used for the arguments (a, b). For example, if the arguments are (3, 2) and (2, 3), two separate calls should be made.
+fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2) otherwise.
+factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
+
+*/
+function memoize(fn) {
+
+    let cache = {}
+    return function (...args) {
+        let n = JSON.stringify(args)
+        if(n in cache){
+            return cache[n]
+        }
+        else{
+            let result = fn.apply(this, args)
+            cache[n] = result;
+            return result;
+        }
+    }
+}m
